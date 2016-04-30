98bd4fed1677b573ec23cc4f3bae3fc18f230c25
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-04-30 17:35:14 -0500

    Add greet endpoint with superb

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ module.exports = api;
 api.get('/hello', function () {
   return 'hello world';
 });
+
+api.get('/greet', function (request) {
+  var superb = require('superb');
+  return request.queryString.name + ' is ' + superb();
+});
